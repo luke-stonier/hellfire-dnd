6be4ab25d742666a2f7043d4f28a8af2f08f5786
lukestonier <lukestonier@hotmail.com>
2026-01-03 00:39:48 +0000

    theming + fonts

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,39 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './fonts.css'
+// import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className={'text-primary'}>Hellfire DnD</h1>
-      <div className="card">
-          <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <button className='btn btn-secondary' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <div>
+            <div className='border border-primary' style={{ padding: 2, marginBottom: 4 }}></div>
+            <div className='d-flex align-items-center justify-content-center'>
+                <h1 className={'display-1 fw-bold text-primary my-0'}>HELLFIRE D</h1>
+                <span className={'text-primary fw-bold display-4 my-0 d-block'}>n</span>
+                <h1 className={'display-1 fw-bold text-primary my-0'}>D</h1>
+            </div>
+            <div className='border border-primary' style={{ padding: 2, marginTop: 4 }}></div>
+        </div>
+
+        <div className='border border-primary mt-2 p-1 rounded-3'>
+            <div className='d-flex align-items-center justify-content-center'>
+                {/*<Link to='/login'>Login</Link>*/}
+            </div>
+        </div>
+        
+      <div className="w-50 mx-auto d-flex flex-column gap-2 mt-4 p-3 rounded-4 border border-info">
+          <button className='btn btn-primary d-flex align-items-center justify-content-center' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+          <button className='btn btn-secondary' onClick={() => setCount((count) => count + 1)}><span>count is {count}</span></button>
           <button className='btn btn-success' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           <button className='btn btn-danger' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           <button className='btn btn-warning' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           <button className='btn btn-info' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           <button className='btn btn-dark' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <button className='btn btn-light' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <button className='btn btn-light' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
